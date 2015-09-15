a0ad40b9003c99f1eb7165c79f1bb48b4845c8d5
太田 智彬 <tomoaki_ota@r.recruit.co.jp>
2015-09-15 16:15:53 +0900

    Implemented carsensor mock

--- src/app.js
+++ src/app.js
@@ -1,27 +1,42 @@
 var Vue = require('vue');
+Vue.use(require('vue-resource'));
+var carComponent = require('./views/car-component.html');
 
-// http://zukan.kids.yahoo.co.jp/animal/kind/mammals/0102.html
-new Vue({
-  el: '#vue-app',
+Vue.component('car-component', {
+  template: carComponent,
 
-  data: {
-    name: 'ザトウクジラ',
-    binomen: 'megaptera novaeangliae',
-    location: '世界中の海域',
-    description: '尾びれの裏側の多様な模様で個体を識別できる。胸びれは長い。季節にともなって回遊し、アラスカのものは冬は出産と育児のため、ハワイなどの暖かい海へ移動する。オスは繁殖期に「歌」をくりかえし歌う。潮吹きを3～4回した後に潜水する。絶滅が危惧（きぐ）されている。',
-    classification: 'クジラ目ナガスクジラ科',
-    image: 'http://i.yimg.jp/i/kids/zukan/photo/animal/mammals/0102/400_400.jpg',
-    friends: [
-      'シロナガスクジラ',
-      'ジンベエザメ',
-      'マッコウクジラ'
-    ]
+  props: ['catalogCar'],
+
+  data() {
+    return {
+      toggle: false
+    }
   },
 
   methods: {
-    makeSounds(e) {
-      alert(`${this.name}ァァァァッッーーーー！！！！`);
-      e.preventDefault();
+    accordion() {
+      this.toggle = !this.toggle;
+      event.preventDefault();
     }
   }
 });
+
+var vm = new Vue({
+  el: '#vue-app',
+
+  data: {
+    catalogCar: {},
+    toggle: false
+  },
+
+  ready() {
+    var url = 'http://www.carsensorlab.net/webapi/V2/catalogSearch/?output=json&brand=' + encodeURIComponent('ランボルギーニ');
+    this.$http.jsonp(url, function (data, status, request) {
+      this.$set('catalogCar', data.catalogCar);
+    }).error(function (data, status, request) {
+      console.error(status);
+    });
+  }
+});
+
+// console.warn('http://jp.vuejs.org/api/directives.html#v-attr');
